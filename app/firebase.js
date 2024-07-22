44b45ca886638da4db1df4f35a2e34485c5183b7
patemit <patelmit8292@gmail.com>
2024-07-22 13:18:34 -0400

    Set firebaseConfigs to env

--- app/firebase.js
+++ app/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAZNO8QSos8bPxeqWyB4X1RcC8glPsJJrU",
-  authDomain: "ellis-test-support.firebaseapp.com",
-  projectId: "ellis-test-support",
-  storageBucket: "ellis-test-support.appspot.com",
-  messagingSenderId: "983792357259",
-  appId: "1:983792357259:web:03c76b59563e4d8344a98e",
-  measurementId: "G-H8HSLQQDGK",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
 };
 
 // Initialize Firebase
